e5407976d555381e29c3687333040a83e3b5e935
David Hecker <heckerdw@gmail.com>
2022-01-08 15:27:13 -0800

    Added create, update, and delete urls, views, and html.

--- snacks/models.py
+++ snacks/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.urls import reverse
 class Snack(models.Model):
 
     title = models.CharField(max_length=256)
@@ -9,3 +9,7 @@ class Snack(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        # return reverse("snack_detail", args=[str(self.id)])
+        return reverse("snack_create")
--- snacks/views.py
+++ snacks/views.py
@@ -1,6 +1,30 @@
+from django.db.models import fields
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
-
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Snack
+from django.urls import reverse_lazy
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class SnackListView(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
+
+class SnackCreateView(CreateView):
+    template_name = 'snack_create.html'
+    model = Snack
+    fields = ['title', 'purchaser', 'description']
+
+class SnackUpdateView(UpdateView):
+    template_name = 'snack_update.html'
+    model = Snack
+    fields = ['title', 'purchaser', 'description']
+
+class SnackDeleteView(DeleteView):
+    template_name = "snack_delete.html"
+    model = Snack
+    success_url = reverse_lazy("snack_list")
